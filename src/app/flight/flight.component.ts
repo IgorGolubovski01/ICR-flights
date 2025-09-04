6866423e029115cd6cb836d2454a7d1d7bb5ddab
IgorGolubovski01 <igor.golubovski.21@singimail.rs>
2025-09-04 14:11:05 +0200

    .

---
+++ src/app/flight/flight.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { FlightModel } from '../../models/flight.model';
+import { WebService } from '../web.service';
+import { JsonPipe, NgIf } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-flight',
+  imports: [JsonPipe, HttpClientModule, RouterLink, NgIf],
+  templateUrl: './flight.component.html',
+  styleUrl: './flight.component.css'
+})
+export class FlightComponent {
+
+  public webService: WebService
+  public flight: FlightModel | null = null
+
+  constructor(private route: ActivatedRoute) {
+    this.webService = new WebService
+
+    route.params.subscribe(params => {
+      const id = params['id']
+
+      this.webService.getFlightById(id)
+        .subscribe(rsp => this.flight = rsp)
+    })
+  }
+}
